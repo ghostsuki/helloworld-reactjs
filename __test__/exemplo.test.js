58cda06562d6508be36e3a629b7098dee6827012
ghostsuki <ghostsukii607@gmail.com>
2025-10-20 14:24:31 -0400

    feat: teste em aula

--- __test__/exemplo.test.js
+++ __test__/exemplo.test.js
@@ -1,7 +1,7 @@
 describe('Grupo de Testes de Exemplo', () => {
 
     it('deve retornar verdadeiro que 1 + 1 é igual a 2', () => {
-        expect(1 + 1).toBe(4);
+        expect(1 + 1).toBe(2);
     });
 
 });
