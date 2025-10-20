9c5c4aa4d06cbe926d7c8bbf02608c2ea39df277
ghostsuki <ghostsukii607@gmail.com>
2025-10-20 13:40:14 -0400

    feat: teste rodando

--- __test__/exemplo.test.js
+++ __test__/exemplo.test.js
@@ -1,12 +1,7 @@
-// arquivo: __tests__/exemplo.test.js
-
-// 'describe' agrupa testes relacionados
 describe('Grupo de Testes de Exemplo', () => {
 
-    // 'it' ou 'test' define um caso de teste individual
     it('deve retornar verdadeiro que 1 + 1 é igual a 2', () => {
-        // 'expect' é a asserção, o que esperamos que aconteça
-        expect(1 + 1).toBe(3);
+        expect(1 + 1).toBe(2);
     });
 
 });
